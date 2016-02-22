f59fe908a673e5fbfc69820aa929ddd9f5bb7ed3
trottomv <trottomv@gmail.com>
2016-02-22 18:48:13 +0100

    Added files via upload

--- caramaestra.rb
+++ caramaestra.rb
@@ -15,19 +15,19 @@ bot.listen do |message|
 		bot.api.sendMessage(chat_id: message.chat.id, text: "Ciao @#{message.from.username}, per iniziare la lezione invia /lezione.")
 	when '/lezione'
 		bot.api.sendMessage(chat_id: message.chat.id, text: "Ciao @#{message.from.username}, benvenut* al corso di grammatica italiana, io sono la prof.ssa Maria Barbadoro.")
-		bot.api.sendMessage(chat_id: message.chat.id, text: "Per dialogare con me occorre utilizzare il simbolo / anteposto ad ogni parola. Tutto chiaro? /si o /no")
+		bot.api.sendMessage(chat_id: message.chat.id, text: "Per dialogare con me occorre utilizzare il simbolo '/' anteposto ad ogni parola. Tutto chiaro? /si o /no")
 		when message.text = '/no'
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Per esempio quando faccio l'appello e chiedo <<@#{message.from.username} e' presente>>? la sintassi corretta della risposta e' /si. Adesso hai capito?")	
 		when message.text = '/si'
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Bene, iniziamo subito con la nostra lezione.")
-			bot.api.sendMessage(chat_id: message.chat.id, text: "Caro @#{message.from.username}, sapresti dirmi quale e' la capitale di Italia?")
+			bot.api.sendMessage(chat_id: message.chat.id, text: "Caro @#{message.from.username}, potresti dirmi quale e' la capitale di Italia?")
 		when message.text = '/roma'
-			bot.api.sendMessage(chat_id: message.chat.id, text: "Come sarebbe a dire <<roma>>?? Intendevi dire <<Roma>> spero??")
+			bot.api.sendMessage(chat_id: message.chat.id, text: "Come sarebbe a dire /roma?? Intendevi dire /Roma spero??")
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Riscrivila correttamente per cortesia.")
 		when message.text = '/Roma'
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Bene prendi pure posto e prestiamo attenzione!!")
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Come prima cosa studiati bene l'uso delle maiuscole nella lingua italiana http://www.grammaticaitaliana.eu/uso_delle_maiuscole.html")
-			bot.api.sendMessage(chat_id: message.chat.id, text: "Fatto? quando sarai pronto rispondi /fatto")
+			bot.api.sendMessage(chat_id: message.chat.id, text: "Fatto? quando sarai pronto rispondi /fatto .")
 		when message.text = '/fatto'
 			bot.api.sendMessage(chat_id: message.chat.id, text: "Bene, domanda: i nomi di popoli, nel caso vengano utilizzati come aggettivi, devono essere scritti con l'iniziale /maiuscola o /minuscola?")
 		when message.text = '/minuscola'
